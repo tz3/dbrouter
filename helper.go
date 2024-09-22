094a1383b1cb5990e0270d3fa6dcac68183b664e
Moutaz <xmoutaz@hotmail.com>
2024-09-22 14:00:30 +0200

    Add initial functionalities

--- helper.go
+++ helper.go
@@ -1,19 +1,20 @@
 package dbrouter
 
+// doConcurrent runs a function concurrently n times and collects any errors.
+// It returns the last non-nil error, if any.
 func doConcurrent(n int, fn func(i int) error) error {
-	errors := make(chan error, n)
+	errors := make(chan error, n) // Channel to collect errors.
 
-	var i int
-	for i = 0; i < n; i++ {
-		go func(i int) { errors <- fn(i) }(i)
+	for i := 0; i < n; i++ {
+		go func(i int) { errors <- fn(i) }(i) // Launch goroutines to execute fn.
 	}
 
-	var err, innerErr error
-	for i = 0; i < cap(errors); i++ {
-		if innerErr = <-errors; innerErr != nil {
-			err = innerErr
+	var err error
+	for i := 0; i < n; i++ {
+		if e := <-errors; e != nil { // Collect errors from all goroutines.
+			err = e
 		}
 	}
 
-	return err
+	return err // Return the last non-nil error, or nil if no errors.
 }
